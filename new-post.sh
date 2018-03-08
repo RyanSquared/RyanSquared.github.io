64519770cd097fc4c11975c3dedfa2224e66d8e7
RyanSquared <vandor2012@gmail.com>
2018-03-08 10:16:56 -0600

    new-post: Initial Commit

---
+++ new-post.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+filename="_posts/$(date '+%Y-%m-%d')-$(echo $@ | tr ' ' '-' | tr A-Z a-z | sed "s/[^a-z\\-]//g")"
+
+cat > $filename <<END
+---
+layout: post
+title:  "$*"
+date:   $(date '+%Y-%m-%d %H:%M:%S %z')
+---
+END
+
+${EDITOR:-vim} "$filename"
