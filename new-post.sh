95f390e2b1ebcafb343c9cd6dcb3cbc0cf02b864
RyanSquared <vandor2012@gmail.com>
2018-03-20 18:19:21 -0500

    new-post: Fix up bug with filename generation

--- new-post.sh
+++ new-post.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-filename="_posts/$(date '+%Y-%m-%d')-$(echo $@ | tr ' ' '-' | tr A-Z a-z | sed "s/[^a-z\\-]//g")"
+filename="_posts/$(date '+%Y-%m-%d')-$(echo $@ | tr ' ' '-' | tr A-Z a-z | sed "s/[^a-z\\-]//g").md"
 
 cat > $filename <<END
 ---
